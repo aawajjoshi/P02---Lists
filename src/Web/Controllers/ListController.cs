e7d3069cee640e2d46160c526c72a4a8987775e4
aawajjoshi <s521315@nwmissouri.edu>
2019-02-08 15:26:06 -0600

    Added a new controller and the required views.

---
+++ src/Web/Controllers/ListController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dotnet_mvc.Models;
+
+namespace dotnet_mvc.Controllers
+{
+    public class ListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ShowText()
+        {
+            return Content("Click back to return to the previous page.");
+        }
+        public IActionResult ShowJSON()
+        {
+            return Json(new { name = "favorite name list", quote = "This is my favorite name list." });
+        }
+        public IActionResult ShowHTML()
+        {
+            var h = "<!DOCTYPE html><html><body>An awesome name list" +
+            "<br><br> <a href='/Quote'>Go Back to List</a></body></html>";
+            return Content(h, "text/html");
+        }
+
+        public IActionResult ShowView()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
