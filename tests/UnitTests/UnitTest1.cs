de1b829c52c134831b4492dc73b1937e55868cad
aawajjoshi <s521315@nwmissouri.edu>
2019-02-08 15:39:18 -0600

    Created and updated test.

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -6,9 +6,37 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Multiply_WorksWithTwoPositiveIntegers()
         {
-
+            var expected = 4;
+            var a = 2;
+            var b = 2; 
+            var actual = multiply(a, b);
+            Assert.Equal(expected,actual);
         }
+
+       
+
+      
+
+        [Theory]
+        [InlineData(4,2,2)]
+        [InlineData(12,4,3)]
+        [InlineData(0, 0,0)]
+        public void Multiply_WorksWithManyInputs(int exp, int n1, int n2)
+        {
+            Assert.Equal(exp, multiply(n1, n2));
+        }
+         
+
+
+        // for more see https://xunit.github.io/docs/getting-started/netcore/cmdline
+
+        int multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+       
     }
 }
